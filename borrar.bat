a70e5cc9ca4845ca8f97176cfefef7e8dc309448
Mankatico <114008682+Mankatico@users.noreply.github.com>
2022-09-20 10:50:39 -0500

    Add files via upload

---
+++ borrar.bat
@@ -0,0 +1,6 @@
+@echo off
+del D:\user\ISER2022\Desktop\archivos\1.txt
+del D:\user\ISER2022\Desktop\archivos\2.txt
+del D:\user\ISER2022\Desktop\archivos\3.txt
+del D:\user\ISER2022\Desktop\archivos\4.txt
+pause
