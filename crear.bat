efc56cf2743e635076ecff289afe9f2682739076
Mankatico <114008682+Mankatico@users.noreply.github.com>
2022-09-20 11:05:12 -0500

    Add files via upload

---
+++ crear.bat
@@ -0,0 +1,6 @@
+@echo off
+tree D:\user\ISER2022\Desktop\archivos> 1.txt
+tree D:\user\ISER2022\Desktop\archivos> 2.txt
+tree D:\user\ISER2022\Desktop\archivos> 3.txt
+tree D:\user\ISER2022\Desktop\archivos> 4.txt
+pause
